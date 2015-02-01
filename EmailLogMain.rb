c2c3ee6319666f9f6a3ae2ee6dfaaf17f2599900
Stephen <stunger@mines.edu>
2015-02-01 13:17:28 -0700

    Now checks for and returns correct number of totla messages sent

--- EmailLogMain.rb
+++ EmailLogMain.rb
@@ -2,12 +2,14 @@ class EmailLogMain
 
 	def load
 		@logList = Array.new
+		ct = 0
 		File.open("mail.log.txt").each do |i|
 			if i =~ /: to=</ 
 				@logList << i
+			elsif i =~ /removed/
+				ct+=1
 			end
 		end
-		@logList.size
+		ct
 	end
-
 end
--- EmailLogTester.rb
+++ EmailLogTester.rb
@@ -5,7 +5,6 @@ class EmailLogTester < Test::Unit::TestCase
 
 	def tests
 		log = EmailLogMain.new
-		assert_equal(69, log.load)		# Checking for correct number of messages
-		
+		assert_equal(61, log.load)		# Checking for correct number of messages
 	end
 end
