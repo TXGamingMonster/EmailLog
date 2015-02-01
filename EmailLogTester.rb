ef54b05b5dc460c8bf1599beb2f2d29d88ec70ec
Stephen <stunger@mines.edu>
2015-02-01 13:26:19 -0700

    Added testing for recipients

--- EmailLogTester.rb
+++ EmailLogTester.rb
@@ -6,5 +6,9 @@ class EmailLogTester < Test::Unit::TestCase
 	def tests
 		log = EmailLogMain.new
 		assert_equal(61, log.load)		# Checking for correct number of messages
+		assert_equal(69, log.getToList.size)	# Checking for correct number of recipients
+		log.getToList.each do |i|
+			assert_true(i.includes('@'))
+		end
 	end
 end
