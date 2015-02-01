28bee45229c6bfb9d3aa30bfd229ef96b0de64c5
Stephen <stunger@mines.edu>
2015-02-01 16:08:39 -0700

    Implemented Hash Table to pair to addresses with specific ID

--- EmailLogMain.rb
+++ EmailLogMain.rb
@@ -10,9 +10,36 @@ class EmailLogMain
 		@logList.size
 	end
 	
-	def getID(logListIndex)
-		id = @loglist[logListIndex].slice((/: [0-9|A-Z]/)+2..(/[0-9|A-Z]: /))
-		return id
+	
+	#Uses RegEx to pull substring, containing message ID, out of string
+	def getID(str)
+		return str[(str=~/: [0-9|A-Z]/)+2..(str=~/[0-9|A-Z]: /)]
 	end
+	
+	def makeTable
+		@table = Hash.new(Array.new)
+		@logList.each do |i|
+			str = getID(i)
+			@table[str] = getTos(str)
+		end
+	end
+	
+	def getTos(str)
+		ary = Array.new
+		File.open("mail.log.txt").each do |i|
+			if((i=~/#{str}/) && (i=~/: to=</))
+				ary << i[(i=~/: to=</)+6..(i=~/>, /)-1]
+			end
+		end
+		if ary.size>1
+			ary.delete_at(0)
+		end
+		return ary
+	end
+	
+	def getTable
+		return @table
+	end
+	
 end
 
--- EmailLogTester.rb
+++ EmailLogTester.rb
@@ -5,7 +5,21 @@ class EmailLogTester < Test::Unit::TestCase
 
 	def tests
 		log = EmailLogMain.new
-		assert_equal(61, log.load)		# Checking for correct number of messages
+		# Checking for correct number of messages
+		assert_equal(61, log.load)
+		
+		log.makeTable
+		table = log.getTable
+		# Asserting that number of message ID's equal number of messages
+		assert_equal(61,table.length)		
+		
+		# Counting number of to addresses in HashTable
+		ct=0
+		table.keys.each do |i|
+			ct+=table[i].length
+		end
+		# Checking for correct number of to addresses
+		assert_equal(69, ct)
 	end
 end
 
