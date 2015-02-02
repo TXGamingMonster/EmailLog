23d24025f39f5f905d3b2f1bd7d75df23413c5a2
Jack Wesley Nelson <janelson@mymail.mines.edu>
2015-02-01 23:33:41 -0700

    added clarifying comments, added more tests for checking id's and number of recipients associated with those id's

--- EmailLogMain.rb
+++ EmailLogMain.rb
@@ -3,7 +3,9 @@ class EmailLogMain
 	def load
 		@logList = Array.new
 		File.open("mail.log.txt").each do |i|
+			#check if reg exp returns non nil
 			if i =~ /size=[0-9]*?,/ 
+				#then add to array
 				@logList << i
 			end
 		end
@@ -16,6 +18,7 @@ class EmailLogMain
 		return str[(str=~/: [0-9|A-Z]/)+2..(str=~/[0-9|A-Z]: /)]
 	end
 	
+	#associate id's with recipients for easy access later, from is easier to fetch
 	def makeTable
 		@table = Hash.new(Array.new)
 		@logList.each do |i|
@@ -23,11 +26,13 @@ class EmailLogMain
 			@table[str] = getTos(str)
 		end
 	end
-	
+
 	def getTos(str)
 		ary = Array.new
 		File.open("mail.log.txt").each do |i|
+			#check if both reg exps return a non nil value
 			if((i=~/#{str}/) && (i=~/: to=</))
+				#then store the substr into array
 				ary << i[(i=~/: to=</)+6..(i=~/>, /)-1]
 			end
 		end
@@ -37,22 +42,31 @@ class EmailLogMain
 		return ary
 	end
 	
+	#returns the table from makeTable
 	def getTable
 		return @table
 	end
 	
 	def print
 		@logList.each do |i|
+			#initialize empty string
 			str = "";
+			#fetch Month and Day
 			str += "DATE: " + i[0..6]
+			#fetch time sent
 			str += "\nTIME: " + i[(i=~/[0-9]*:[0-9]*:[0-9]* /)..(i=~/toilers/)-2]
+			#fetch size of message
 			str += "\nSIZE: " + i[(i=~/size=/)+5..(i=~/, nrcpt/)-1]
+			#fetch ID
 			str += "\nID: " + getID(i)
+			#fetch Sender
 			str += "\nFROM: " + i[(i=~/from=</)+6..(i=~/>, s/)-1]
+			#fetch recipient(s)
 			str += "\nTO: "
 			@table[getID(i)].each do |j|
 				str += j+", "
 			end
+			#print formatted log data
 			puts "\n"+str[0..-3]
 		end
 	end
--- EmailLogTester.rb
+++ EmailLogTester.rb
@@ -13,6 +13,19 @@ class EmailLogTester < Test::Unit::TestCase
 		# Asserting that number of message ID's equal number of messages
 		assert_equal(61,table.length)		
 		
+		#check if id's were stored as keys, and recipient number at key
+		#top
+		assert_equal(true, table.has_key?("A54F11B2003"))
+		assert_equal(1, table.values_at("A54F11B2003")[0].length)
+		#mid
+		assert_equal(true, table.has_key?("168191BA094"))
+		assert_equal(1, table.values_at("168191BA094")[0].length)
+		#last
+		assert_equal(true, table.has_key?("9E59D1BA094"))
+		assert_equal(1, table.values_at("9E59D1BA094")[0].length)
+		#something with more than 1 recipient
+		assert_equal(3, table.values_at("1915B1B2004")[0].length)
+		
 		# Counting number of to addresses in HashTable
 		ct=0
 		table.keys.each do |i|
